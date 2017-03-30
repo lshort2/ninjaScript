8448250aa8625ac6d4e53bd07f72aed9efbfa562
Lukey <lshort2@mail.csuchico.edu>
2017-03-30 12:27:39 -0700

    started making fireball rotate

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,30 @@
+var oldX =10;
+var oldY =10;
+var rot  =0;
+
 function moveFire(objy) {
   var x = event.clientX;
   var y = event.clientY;
+
+  var slope = (oldY - y)/(oldX - x);
+  slope = Math.atan(slope) * 57.2;
+  if(x < oldX) {
+    slope+=180;
+  }
+  rot+= slope;
+
   move(objy) 
     .x(x-60) 
     .y(y-32) 
+    .rotate(slope)
     .end();
+  oldX =x;
+  oldY =y;
+
+  blowShitUp();
+}
+
+function blowShitUp() {
+  //code
 }
 
